f779e32c9e12c07be0944763b5a5abb18f59b045
Rodrigo <jaandrohernandez@gmail.com>
2019-08-28 14:17:08 -0600

    Importancion
    Jeje

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,11 @@ import  {HomeComponent } from './components/home/home.component';
 import { RegisterComponent } from './components/register/register.component';
 import { RegisterAdminComponent } from './components/register-admin/register-admin.component';
 import { UsuariosComponent } from './components/usuarios/usuarios.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
+  {path:'', redirectTo:'login', pathMatch:'full'},
+  {path: 'login', component: LoginComponent},
   {path: 'home', component: HomeComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'registerAdmin', component: RegisterAdminComponent},
