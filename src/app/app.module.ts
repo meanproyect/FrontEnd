0845ca9db79e8504372e77521368424d9d510eff
Rodrigo <jaandrohernandez@gmail.com>
2019-08-28 10:35:10 -0600

    Ventana principal y ventana de registro
    Diseño Front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { RegisterProductosComponent } from './components/register-productos/register-productos.component';
-import { ProductosComponent } from './components/productos/productos.component';
-import { CarrritoComponent } from './components/carrrito/carrrito.component';
+
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { UsuariosComponent } from './components/usuarios/usuarios.component';
@@ -18,9 +16,6 @@ import { HomeComponent } from './components/home/home.component';
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    RegisterProductosComponent,
-    ProductosComponent,
-    CarrritoComponent,
     NavbarComponent,
     ProfileComponent,
     UsuariosComponent,
