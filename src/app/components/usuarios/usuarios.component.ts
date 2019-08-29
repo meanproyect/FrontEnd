da5fbce6fd3844da5fd4446cdb490676ff15eeb9
Rodrigo <jaandrohernandez@gmail.com>
2019-08-29 09:15:12 -0600

    Listar Clientes

--- src/app/components/usuarios/usuarios.component.ts
+++ src/app/components/usuarios/usuarios.component.ts
@@ -2,16 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { ClientService } from 'src/app/services/client.service';
 import { ClientModels } from 'src/app/models/client-models';
 
+
 @Component({
   selector: 'app-usuarios',
   templateUrl: './usuarios.component.html',
   styleUrls: ['./usuarios.component.css']
 })
 export class UsuariosComponent implements OnInit {
+  client: ClientModels;
+  clientes: ClientModels[];
 
-  constructor() { }
+  constructor(public rest: ClientService) { }
 
   ngOnInit() {
+    this.getClients();
+  }
+
+  
+  getClients() {
+    this.rest.getClients().subscribe(res => {
+      console.log(res);
+      this.clientes = res.client
+    })
   }
 
 }
