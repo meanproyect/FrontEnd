038e91eec2090cd3a8159eea780084ea8f01ae8e
Kevin Román <kevin.romane99@gmail.com>
2019-08-30 14:24:38 -0300

    login

---
+++ src/app/models/login-models.spec.ts
@@ -0,0 +1,7 @@
+import { LoginModels } from './login-models';
+
+describe('LoginModels', () => {
+  it('should create an instance', () => {
+    expect(new LoginModels()).toBeTruthy();
+  });
+});
---
+++ src/app/models/login-models.ts
@@ -0,0 +1,7 @@
+export class LoginModels {
+    constructor(
+        public code: string,
+        public password: string
+    ){}
+}
+
