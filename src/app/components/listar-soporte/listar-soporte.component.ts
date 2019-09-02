34d4375b2f0f117d5edfbe52d2d63d078e5a16b2
Rodrigo <jaandrohernandez@gmail.com>
2019-09-02 10:27:24 -0600

    Listar Soportes

--- src/app/components/listar-soporte/listar-soporte.component.ts
+++ src/app/components/listar-soporte/listar-soporte.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { SupportService } from 'src/app/services/support.service';
+import { SupportModel } from 'src/app/models/support-model';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-listar-soporte',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listar-soporte.component.css']
 })
 export class ListarSoporteComponent implements OnInit {
+  support: SupportModel;
+  supports = [];
+  search: string;
 
-  constructor() { }
+
+  constructor(public rest: SupportService, private router: Router) { }
 
   ngOnInit() {
+    this.getSupports();
+  }
+
+  getSupports(){
+    this.rest.getSupport().subscribe(res => {
+      console.log(res);
+      this.supports = res.support;
+    });
   }
 
 }
