5304dc88cc30565a0f56e35052b50ad3786ba559
Rodrigo <jaandrohernandez@gmail.com>
2019-09-03 10:43:48 -0600

    LogOut Home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import * as jwt_decode from 'jwt-decode';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  public islogged: boolean = false;
+  public token = localStorage.getItem('token');
+  public token1 =  jwt_decode(this.token);
+  public rol = this.token1.role;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.getAuth();
+    console.log(this.rol);
+  }
+
+  onLogout(){
+    localStorage.setItem('auth', 'false');
+    this.router.navigateByUrl('login');
+    this.getAuth();
+  }
+
+  getAuth(){
+    if(localStorage.getItem('auth') == 'true'){
+      this.islogged = true;
+      
+    }else{
+      this.islogged = false;
+      
+    }
   }
 
 }
